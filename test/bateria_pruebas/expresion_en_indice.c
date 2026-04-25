cfae8756b899d4d1539ecff8fcd1779f149d580f
100499101 <100499101@alumnos.uc3m.es>
2026-04-25 21:08:22 +0200

    pruebas actualizadas

--- test/bateria_pruebas/expresion_en_indice.c
+++ test/bateria_pruebas/expresion_en_indice.c
@@ -6,18 +6,21 @@ main() {
     puts("Rellenando vector (0, 10, 20, 30, 40):");
     while (i < 5) {
         b[i] = i * 10;
-        printf("Guardado en b[%d]: %d\n", i, b[i]);
+        puts(" ");
+        printf("", "Guardado en la posición: ", i, ", el valor: ", b[i]);
         i = i + 1;
     }
 
-    printf("Ultimo elemento (i-1): %d\n", b[i - 1]);
+    puts(" ");
+    printf("", "Ultimo elemento (i-1): ", b[i - 1]);
     
     b[i - 5] = 999;
 
     puts("Vector tras modificar la posicion 0:");
     i = 0;
     while (i < 5) {
-        printf("b[%d] = %d\n", i, b[i]);
+        puts(" ");
+        printf("", "Guardado en la posición: ", i, ", el valor: ", b[i]);
         i = i + 1;
     }
 }
--- test/bateria_pruebas/operaciones_aritmeticas.c
+++ test/bateria_pruebas/operaciones_aritmeticas.c
@@ -4,18 +4,24 @@ int res = 0;
 
 main() {
     puts("Probando operaciones aritméticas:");
+
+    puts(" ");
     res = x + y;
     printf("%d", res);
     
+    puts(" ");
     res = x - y;
     printf("%d", res);
-
+    
+    puts(" ");
     res = x * y;
     printf("%d", res);
-
+    
+    puts(" ");
     res = x / y;
     printf("%d", res);
-
+    
+    puts(" ");
     res = x % y;
     printf("%d", res);
 }
